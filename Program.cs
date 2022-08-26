5a8689b75f81d979d3a95ff07eb81d001a298366
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-27 00:56:16 +0530

    Fitering contacts using city and state

--- Program.cs
+++ Program.cs
@@ -1,7 +1,59 @@
-﻿namespace AddressBookSystem
+﻿using System.Collections;
+
+namespace AddressBookSystem
 {
     class Program
     {
+        public static void FilterByCityAndState()
+        {
+            
+            foreach (var kv in addressBookSystem)
+            {
+                foreach(Contact contact in kv.Value)
+                {
+                    // City filtering
+
+                    //check city is added into city dictionary?
+                    if(cityDict.ContainsKey(contact.city))
+                    {
+                       //add contact entry into exiting key- value (city-contacts) list
+                       cityDict[contact.city].Add(contact);
+
+                    }
+                    else
+                    {
+                        //adding new entery of city  key-value pair
+                        cityDict.Add(contact.city, new List<Contact>());
+                        //adding contact entry into created key city
+                        cityDict[contact.city].Add(contact);
+                    }
+
+                    //state filtering
+                    
+                    //check state is added into state dictionary?
+                    if (stateDict.ContainsKey(contact.state))
+                    {
+                        //add contact entry into exiting key- value (city-contacts) list
+                        stateDict[contact.state].Add(contact);
+
+                    }
+                    else
+                    {
+                        //adding new entery of city
+                        stateDict.Add(contact.state, new List<Contact>());
+                        stateDict[contact.state].Add(contact);
+                    }
+                }
+            }
+
+            DisplayDictionary(cityDict);
+            DisplayDictionary(stateDict);
+
+        }
+
+
+        
+
         public static void SearchByCityOrState()
         {
 
@@ -189,12 +241,12 @@
             }//while end
         }
 
-        public static void DisplayDictionary()
+        public static void DisplayDictionary(Dictionary<string,List<Contact>> dict)
         {
-            Console.WriteLine("Diplay current data in addressbook: ");
-            foreach (KeyValuePair<string, List<Contact>> obj in addressBookSystem)
+            Console.WriteLine("Diplay current data in dictionary: ");
+            foreach (KeyValuePair<string, List<Contact>> obj in dict)
             {
-                Console.WriteLine("Displaying contacts of adressbook {0}", obj.Key);
+                Console.WriteLine("Displaying contacts of {0}", obj.Key);
                 DisplayContacts(obj.Value);
                 Console.WriteLine("===============================================");
             }
@@ -214,14 +266,15 @@
                 addressBookSystem.Add(name, addressBook);
 
                 CreatingContacts(addressBook);
-
+                /*
                 if (addressBook.Count > 0)
                 {
                     EditContacts(addressBook);
                     DeleteContacts(addressBook);
                 }
-                DisplayDictionary();
-
+                
+                DisplayDictionary(addressBookSystem);
+                */
                 Console.WriteLine("Do you want to create another addressbook press 1 or press 2 for exit:");
                 num = Convert.ToInt32(Console.ReadLine());
             }
@@ -230,13 +283,17 @@
 
 
         public static Dictionary<string, List<Contact>> addressBookSystem = new Dictionary<string, List<Contact>>();
+       public static Dictionary<String, List<Contact>> cityDict = new Dictionary<string, List<Contact>>();
+       public static Dictionary<String, List<Contact>> stateDict = new Dictionary<string, List<Contact>>();
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Sytem.");
 
             CreateAddresBook();
-            DisplayDictionary();
-            SearchByCityOrState();
+            DisplayDictionary(addressBookSystem);
+            //SearchByCityOrState();
+            FilterByCityAndState();
             //DisplayContacts();
             //EditContacts();
             //DeleteContacts();            
